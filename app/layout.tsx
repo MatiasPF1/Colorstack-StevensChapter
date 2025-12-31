ceb33cc0b189de693f996655116b21681e290aea
Matias Freire <matiaspfreire@gmail.com>
2025-12-31 16:33:44 -0500

    Navbar section - Working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,42 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next";  
+import { Inter } from "next/font/google"; // Import For the Fonts 
+import "./globals.css"; // Global CSS import
+import Navbar from "./components/Navbar";
 
-export const metadata: Metadata = {
+//1- Font Configuration For the Text
+
+const inter = Inter({ 
+  subsets: ["latin"],                //Only Accept Latin Chars 
+  display: "swap",                   // Fallback Is shown until Inter Loads, then swaps
+  fallback: ["Arial", "sans-serif"], // Options if Inter Fails to Load 
+});
+
+
+//2- Metadata: Title and Description
+
+export const metadata: Metadata = { 
+  // Metadata: Title and Description
   title: "ColorStack Stevens Chapter",
-  description: "ColorStack Stevens Institute of Technology Chapter",
+  description: "ColorStack Stevens Institute of Technology Chapter", 
 };
 
-export default function RootLayout({
+//3- Root Layout Component
+
+
+export default function RootLayout({ // Wrapping Layout Component
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) 
+{
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className} suppressHydrationWarning> {/* Apply Google Font to Body(inter)*/}
+
+        <Navbar/>  {/*1st Part of the Website Layout*/}
+
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <h1>ColorStack Stevens Chapter</h1>
+    <main className="">
     </main>
   );
 }
