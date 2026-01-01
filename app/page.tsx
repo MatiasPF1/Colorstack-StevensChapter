43d5b997ffaf55a932e3a26c6e98c9180928d21b
Matias Freire <matiaspfreire@gmail.com>
2026-01-01 14:50:31 -0500

    Integration of Conctact-Us in the UI)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/about";
 import Main_Idea from "./components/Main_Idea";
+import ContactPage from "./contactus/page";
 
 export default function Home() {
   return (
     <main className="">
       <About/>
       <Main_Idea/>
+      <ContactPage/>
     </main>
   );
 }
