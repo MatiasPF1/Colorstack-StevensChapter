e26d02e7f5d71a26d932e7b3a0b82776761734d7
Matias Freire <matiaspfreire@gmail.com>
2026-01-01 21:50:16 -0500

    re design of the contacpage

--- app/Home_Components/Contactpage.tsx
+++ app/Home_Components/Contactpage.tsx
@@ -19,34 +19,34 @@ export default function ContactPage(){
 			</h1>
 
 			{/* Contact Options Grid and its options: */}
-			<div className ="grid grid-cols-3 auto-rows-fr gap-4 mt-15! ">
+			<div className ="grid grid-cols-2 auto-rows-fr gap-9 mt-16! mx-30! mb-20!">
 
                 {/* Instagram */}
-				<div className = "flex flex-col items-center justify-center gap-4 bg-white rounded-2xl shadow-lg "> 
-					<h2 className = "flex text-[#E10600] items-center justify-center font-bold text-xl mt-10!">Instagram</h2>
-					<p className = "text-gray-600 text-center w-fit">Follow us for updates, events, and <br/> community highlights</p>
-					<button onClick= {()=>(window.open(socials["instagram" as Platform], "_blank"))} className = "bg-[#E10600] rounded-3xl text-white px-6! py-2! mb-10!">Follow Us</button>
+				<div className = "flex flex-col items-center justify-center gap-2 bg-white rounded-2xl shadow-lg "> 
+					<h2 className = "flex text-[#c41230] items-center justify-center font-bold text-xl mt-10!">Instagram</h2>
+					<p className = "text-gray-600 text-center ">Follow us for updates, events, and <br/> community highlights</p>
+					<button onClick= {()=>(window.open(socials["instagram" as Platform], "_blank"))} className = "bg-[#c41230] rounded-3xl text-white px-7! py-3! mb-10! ">Follow Us</button>
 				</div>
 
  			   {/* Discord */}
-				<div className = "flex flex-col items-center justify-center gap-4 bg-white rounded-2xl shadow-lg "> 
-					<h2 className = "flex text-[#E10600] items-center justify-center font-bold text-xl">Discord</h2>
+				<div className = "flex flex-col items-center justify-center gap-2 bg-white rounded-2xl shadow-lg "> 
+					<h2 className = "flex text-[#c41230] items-center justify-center font-bold text-xl mt-10!">Discord</h2>
 					<p className = "text-gray-600 text-center">Join our Discord server to chat with<br/> members and get real-time updates</p>
-					<button onClick = {()=>(window.open(socials["discord" as Platform], "_blank"))} className = "bg-[#E10600] rounded-3xl text-white px-6! py-2! ">Join Discord</button>
+					<button onClick = {()=>(window.open(socials["discord" as Platform], "_blank"))} className = "bg-[#c41230] rounded-3xl text-white px-7! py-3! mb-10! ">Join Discord</button>
 				</div>
 
                 {/* Email */}
-				<div className = "flex flex-col items-center justify-center gap-4 bg-white rounded-2xl shadow-lg "> 
-					<h2 className = "flex text-[#E10600] items-center justify-center font-bold text-xl">Contact Us</h2>
+				<div className = "flex flex-col items-center justify-center gap-2 bg-white rounded-2xl shadow-lg "> 
+					<h2 className = "flex text-[#c41230] items-center justify-center font-bold text-xl mt-10!">Contact Us</h2>
 					<p className = "text-gray-600 text-center">Have questions? <br/>Reach out to us via email</p>
-					<button onClick = {()=>(window.open(socials["email" as Platform],"_blank"))} className = "bg-[#E10600] rounded-3xl text-white px-6! py-2! ">Send email</button>
+					<button onClick = {()=>(window.open(socials["email" as Platform],"_blank"))} className = "bg-[#c41230] rounded-3xl text-white px-7! py-3! mb-10! ">Send email</button>
 				</div>
 
 	      		{/* National ColorStack */}
-				<div className = " col-start-2 flex flex-col items-center justify-center gap-4 bg-white rounded-2xl shadow-lg ">
-					<h2 className = "flex text-[#E10600] items-center justify-center font-bold text-xl">Join National ColorStack</h2>
+				<div className = " flex flex-col items-center justify-center gap-2 bg-white rounded-2xl shadow-lg ">
+					<h2 className = "flex text-[#c41230] items-center justify-center font-bold text-xl mt-10!">Join National ColorStack</h2>
 					<p className = "text-gray-600 text-center">Acess opprtunties, mentorship, and <br/>join 10,000+ members</p>
-					<button onClick = {()=>(window.open(socials["website" as Platform],"_blank"))} className = "bg-[#E10600] rounded-3xl text-white px-6! py-2! ">Apply Now</button>
+					<button onClick = {()=>(window.open(socials["website" as Platform],"_blank"))} className = "bg-[#c41230] rounded-3xl text-white px-7! py-3! mb-10! ">Apply Now</button>
 				</div>
 		
 			</div>
