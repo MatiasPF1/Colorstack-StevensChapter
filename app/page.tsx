f992665d5b899e5a3847475dee1b071b74095a6c
Alvaro-bit-stack <aizquierdo1325@gmail.com>
2026-01-03 17:38:19 -0500

    implemented Mission section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import About from "./Home_Components/about";
 import Main_Idea from "./Home_Components/Main_Idea";
 import ContactPage from "./Home_Components/Contactpage";
-
+import Mission from "./Home_Components/Mission";
 export default function Home() {
   return (
     <main className="">
       <About/>
       <Main_Idea/>
+      <Mission/>
       <ContactPage/>
+      
     </main>
   );
 }
