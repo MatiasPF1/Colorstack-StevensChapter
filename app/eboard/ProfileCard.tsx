3c3662409d39c2e49c88d00372c8d75721123315
Matias Freire <matiaspfreire@gmail.com>
2026-01-05 13:49:59 -0500

    cards updated - Version 1

--- app/eboard/ProfileCard.tsx
+++ app/eboard/ProfileCard.tsx
@@ -1,14 +1,26 @@
 import {FaLinkedinIn} from "react-icons/fa";
 
-export default function ProfileCard({image,role,description,linkedin}){
+{/*Typescript Interface for ProfileCard component props */}
+interface ProfileCardProps {
+    image: string;
+    role: string;
+    description: string;
+    linkedin: string;
+}
+
+export default function ProfileCard({image,role,description,linkedin}: ProfileCardProps){
+	// Main container for the profile card
 	return(
-		<div className = "flex flex-col bg-white w-full rounded-xl shadow-xl items-center justify-center gap-2">
-			<div className = "relative w-fit group !mt-5">	
-				<img src= {image} className = "rounded-full aspect-square w-45 object-cover border-3 border-red-500 group-hover:border-red-700 z-0"/>
+		// Container for all the cards content 
+		<div className = "flex flex-col bg-white w-full rounded-xl shadow-xl items-center justify-center gap-2 ">
+			{/* Container for the profile image and the LinkedIn icon overlay */}
+			<div className = "relative w-fit group mt-5! ">	
+				<img src= {image} className = "rounded-full aspect-square w-45 object-cover border-3 border-red-500 group-hover:border-red-700 z-0 "/>
+				{/* Container for the LinkedIn icon overlay positioned absolutely */}
 				<div className = "absolute bg-red-600 group-hover:bg-red-700 w-7 h-7  rounded-lg aspect-square top-5 right-1 z-20 flex items-center justify-center"><a href = {linkedin} className = "flex bg-white w-4 h-4 aspect-square items-center justify-center"><FaLinkedinIn className="text-red-600 w-3 h-3 z-0" /></a></div>
 			</div>
 			<h3 className = "text-red-500 font-bold">{role}</h3>
-			<p className = "text-gray-400 text-md !mb-5">{description}</p>
+			<p className = "text-gray-400 text-md mb-5!">{description}</p>
 					
 		</div>
 	);
--- app/eboard/page.tsx
+++ app/eboard/page.tsx
@@ -1,63 +1,62 @@
-
-import ProfileCard from "./ProfileCard.tsx"
+import ProfileCard from "./ProfileCard"
 import { FaLinkedinIn } from "react-icons/fa";
 
 export default function Eboard(){
 	
 	const people = [
   	{
-    	image: "images/p1.jpg",
+    	image: "images/p8.png",
     	role: "President",
     	description: "Senior · CS",
+    	linkedin: "https://www.linkedin.com/in/carlihannysanchez/",
+  	},
+  	{
+    	image: "images/p7.png",
+    	role: "Vice President",
+    	description: "Junior · Systems Engineering",
+    	linkedin: "https://www.linkedin.com/in/luis-alejandro-ruiz-20xx/",
+  	},
+  	{
+    	image: "images/p4.png",
+    	role: "Head of Outreach",
+    	description: "Sophomore · CS",
     	linkedin: "https://www.linkedin.com/in/matias43/",
   	},
-  	{
-    	image: "images/p2.png",
-    	role: "Vice President",
-    	description: "Junior · Software Engineering",
-    	linkedin: "https://www.linkedin.com/in/example/",
-  	},
-  	{
-    	image: "images/p3.png",
-    	role: "Treasurer",
-    	description: "Sophomore · Data Science",
-    	linkedin: "https://www.linkedin.com/in/example2/",
-  	},
 	{
-    	image: "images/p4.png",
-    	role: "Treasurer",
-    	description: "Sophomore · Data Science",
-    	linkedin: "https://www.linkedin.com/in/example2/",
-  	},
-	{
-    	image: "images/p5.jpg",
-    	role: "Treasurer",
-    	description: "Sophomore · Data Science",
-    	linkedin: "https://www.linkedin.com/in/example2/",
+    	image: "images/p9.jpg",
+    	role: "Head of Technology",
+    	description: "Sophomore · CS",
+    	linkedin: "https://www.linkedin.com/in/alvaro-izquierdo1/",
   	},
 	{
     	image: "images/p6.png",
     	role: "Treasurer",
-    	description: "Sophomore · Data Science",
-    	linkedin: "https://www.linkedin.com/in/example2/",
+    	description: "Sophomore · CS",
+    	linkedin: "https://www.linkedin.com/in/naidelyn-de-la-rosa-250820361/",
   	},
 	{
-    	image: "images/p7.png",
-    	role: "Treasurer",
-    	description: "Sophomore · Data Science",
-    	linkedin: "https://www.linkedin.com/in/example2/",
+    	image: "images/p5.jpg",
+    	role: "Secretyary",
+    	description: "Junior · Quantitative Finance ",
+    	linkedin: "https://www.linkedin.com/in/chiaraplata/",
   	},
 	{
-    	image: "images/p8.png",
-    	role: "Treasurer",
-    	description: "Sophomore · Data Science",
-    	linkedin: "https://www.linkedin.com/in/example2/",
+    	image: "images/p3.png",
+    	role: "Public Relations",
+    	description: "Freshman · Quantitative Finance",
+    	linkedin: "https://www.linkedin.com/in/gregorio-rg/",
   	},
 	{
-    	image: "images/p9.jpg",
-    	role: "Treasurer",
-    	description: "Sophomore · Data Science",
-    	linkedin: "https://www.linkedin.com/in/example2/",
+    	image: "images/p1.jpg",
+    	role: "Freshman Representative",
+    	description: "Freshman · CS",
+    	linkedin: "https://www.linkedin.com/in/juliana-matos-220b62247/",
+  	},
+	{
+    	image: "images/p2.png",
+    	role: "Freshman Representative",
+    	description: "Freshman · CS",
+    	linkedin: "https://www.linkedin.com/in/grant-dibiase-ba6b52382/",
   	},
 
 
@@ -66,7 +65,7 @@ export default function Eboard(){
 	];
 	return(
 	<>
-		<h2 className= "flex justify-center font-bold text-3xl !mb-10">Our Team</h2>
+		<h2 className= "flex justify-center font-bold text-3xl mb-10!">Our Team - 2025/2026 E-Board</h2>
 		<div className = "flex justify-center">
 			<div className = "grid grid-cols-3 gap-4 w-1/2">
 				{people.map((person, index) => (
