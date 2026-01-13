44a7dfe6c63e4b1e14afa002e33c7c2ff8fea293
Matias Freire <matiaspfreire@gmail.com>
2026-01-13 00:11:14 -0500

    interface we forgot to add

--- app/Resources_Components/Tab.tsx
+++ app/Resources_Components/Tab.tsx
@@ -1,11 +1,19 @@
 
-export default function Tab({ label, value, activeTab, setActiveTab }) {
+interface TabProps {
+  label: string;
+  value: string;
+  activeTab: string;
+  setActiveTab: (value: string) => void;
+}
+
+
+export default function Tab({ label, value, activeTab, setActiveTab }: TabProps) {
   const isActive = activeTab === value;
 
   return (
     <button
   	onClick={() => setActiveTab(value)}
-  	className={`!px-4 !py-2 text-lg font-medium rounded-xl transition
+  	className={`px-4! py-2! text-lg font-medium rounded-xl transition
     		${isActive ? "border-2 border-red-500 text-red-500": "border-2 border-transparent text-gray-500 hover:border-black hover:text-black"}`}>
   	{label}
     </button>
